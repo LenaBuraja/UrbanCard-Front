eba4935b94b1993bbc797d9b0fe58f82d924604c
Lena <lenaLatkovich@gmail.com>
2018-03-10 17:02:09 +0300

    Some more refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,18 @@ class App extends React.Component {
 	static propTypes = {
 		store: PropTypes.object.isRequired,
 		routes: PropTypes.object.isRequired
-	}
+	};
 
-	shouldComponentUpdate() {
-		return false;
+	constructor() {
+		super();
+		this.shouldComponentUpdate = () => false;
 	}
 
 	render() {
 		return (
 			<Provider store={this.props.store}>
 				<div style={{ height: "100%" }}>
-					<Router history={browserHistory} children={this.props.routes}/>
+					<Router history={browserHistory} children={this.props.routes} />
 				</div>
 			</Provider>
 		);
