dcff4d6804e1bc8a17e4d3b23ecc5d2003d866a6
LenaLatkovich <lenalatkovich@gmail.com>
2018-03-11 16:37:50 +0300

    Fix active links className

--- app/components/PageLayout.js
+++ app/components/PageLayout.js
@@ -5,9 +5,9 @@ import PropTypes from "prop-types";
 export const PageLayout = ({ children }) => (
 	<div className='page-layout'>
 		<h1>UrbanCard</h1>
-		<IndexLink to='/' activeClassName='.active-nav'>Home</IndexLink>
+		<IndexLink to='/' activeClassName='active-nav'>Home</IndexLink>
 		{"   "}
-		<Link to='/counter' activeClassName='.active-nav'>Counter</Link>
+		<Link to='/counter' activeClassName='active-nav'>Counter</Link>
 		<div className='viewport'>
 			{children}
 		</div>
